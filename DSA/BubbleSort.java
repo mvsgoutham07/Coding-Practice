ca984c0274b0cd6895e0c978ed058ef253d6de9a
M. V. S. GOUTHAM <mvs.goutham@gmail.com>
2026-06-15 15:06:11 +0530

    Add files via upload

---
+++ DSA/BubbleSort.java
@@ -0,0 +1,40 @@
+// Bubble Sort
+
+import java.util.*;
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i=0; i<n-1; i++) {
+            for (int j=0; j<n-i-1; j++) {
+                if (arr[j]>arr[j+1]) {
+                    int x = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = x;
+                }
+            }
+        }
+    }
+    
+    public static void display(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      
+        System.out.print("Enter number of array elements : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+       
+        System.out.println("Enter array elements : ");
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        bubbleSort(arr);
+        System.out.print("Sorter Array : ");
+        display(arr);
+        sc.close();
+    }
+}
