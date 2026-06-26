5e147c1092fd294efe8081d5eac0e27e75af4423
M. V. S. GOUTHAM <mvs.goutham@gmail.com>
2026-06-26 20:49:45 +0530

    Add files via upload

---
+++ Python Practice/Butterfly Number Pattern.py
@@ -0,0 +1,8 @@
+# Butterfly Number Pattern
+
+n = int(input("Enter a number : "))
+for i in range(1, n + 1):
+    left = "".join(str(x) for x in range(1, i + 1))
+    middle = " " * (2 * (n - i))
+    right = "".join(str(x) for x in range(i, 0, -1))
+    print(left + middle + right)
